bf9b6ba8a3a7f1c19fe197be88acf200247c7baa
kahtan-Aldioub <kahtanaldaioub@gmail.com>
2023-10-14 01:12:06 +0300

    margin between news fix

--- components/NewsCard.tsx
+++ components/NewsCard.tsx
@@ -9,7 +9,7 @@ const MovieCard: React.FC<NewsCardProps> = ({ data }) => {
   const router = useRouter();
   return (
     <>
-<div className="mx-auto my-18 flex w-[90vw]  md:w-[60vw] flex-col rounded-lg bg-green-100 font-serif text-black shadow-xl justify-center align-middle">
+<div className="mx-auto my-8 flex w-[90vw]  md:w-[60vw] flex-col rounded-lg bg-green-100 font-serif text-black shadow-xl justify-center align-middle">
   <div className="mt-4">
     <h1 className="mx-11 my-1 font-bold text-[20px] text-green-700 text-center">{data.title}</h1>
   </div>
